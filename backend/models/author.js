5c8d15649131f68c1e17f8b5693ea1c8adc98cb5
Shehab El-Deen Alalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-05-17 01:45:28 +0200

    Author books are now cascade deleted  + enhancing Author

--- backend/models/author.js
+++ backend/models/author.js
@@ -1,12 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bookModel = require("./book");
 
-const authorSchema = new mongoose.Schema({
-    photo: { type: String },
-    firstName: { type: String },
-    lastName: { type: String },
-    dateOfBirth: { type: Date },
-    description: { type: String }
+const authorSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    photo: {
+      type: Buffer,
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// post hook to cascade delete author books on deleting the author
+authorSchema.post("remove", async function (doc, next) {
+    const deletedAuthor = this;
+    await bookModel.deleteMany({ authorId: deletedAuthor._id });
+    return next();
 });
 
-const Authers = mongoose.model('authors', authorSchema);
-module.exports = Authers;
+const Author = mongoose.model("Author", authorSchema);
+module.exports = Author;
