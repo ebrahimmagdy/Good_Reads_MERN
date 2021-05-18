adf3f72ec967284e4081f1d6aaa244c9659bd648
ebrahimmagdy <ebrahim.magdy47@gmail.com>
2021-05-19 01:02:00 +0200

    handle merging

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-<<<<<<< HEAD
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
@@ -30,22 +29,6 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
-=======
-    firstName: { type: String, required: "Your first Name is required" },
-    lastName: { type: String, required: "Your last Name is required" },
-  
-    email: {
-        type: String, trim: true, lowercase: true, unique: true,
-        required: 'Your email address is required',
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill in a valid email address']
-    },
-    password: { type: String, required: "your password is required" },
-    photo: { type: String },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
->>>>>>> d2f72ddb10d3e28318a00a5b905c7b7a06cc9eb5
 });
 
 userSchema.pre("save", function (next) {
---
+++ backend/routes/userBook.js
@@ -0,0 +1,79 @@
+const { request, response } = require('express');
+const express = require('express');
+const Router = express.Router();
+const UserBook = require("../models/userBook");
+const bcrypt = require('bcrypt')
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken')
+const jwt_functions = require("../helper/jwt_functions") 
+
+Router.post("/", jwt_functions.isAuthenticated, async (request, response, next) => {
+    try{
+        const userBookData = request.body
+        const userBookInstance = new UserBook({
+            shelve: userBookData.shelve,
+            userId: userBookData.userId,
+            bookId: userBookData.bookId,
+        })
+        const userBook = await userBookInstance.save()
+        console.log(userBook);
+        response.send("userBook created")
+    } catch (e){
+        console.log(e);
+        response.status(500).send("Error happend!")
+    }
+})
+
+Router.get("/", jwt_functions.isAuthenticated, async (request, response) => {
+    try{
+        const userBooks = await UserBook.find().populate("userId").populate("bookId")
+        response.json(userBooks)
+    } catch (e){
+
+    }
+})
+
+Router.get("/:id", jwt_functions.isAuthenticated, async (request, response) => {
+    try{
+        const id = request.params.id
+        const userBook = await UserBook.findById(id).populate("userId").populate("bookId")
+        response.json(userBook)
+    } catch (e){
+
+    }
+})
+
+Router.get("/user/:id", jwt_functions.isAuthenticated, async (request, response) => {
+    try{
+        const id = request.params.id
+        const userBook = await UserBook.find({userId: id}).populate("userId").populate("bookId")
+        response.json(userBook)
+    } catch (e){
+
+    }
+})
+
+Router.patch("/update/:id", jwt_functions.isAuthenticated, async (request, response) => {
+    try{
+        const id = request.params.id
+        console.log(`update spicefic userBook with id = ${id}`);
+        const userBookData = request.body
+        const userBook = await UserBook.findByIdAndUpdate(id, userBookData)
+        console.log(userBook);
+        response.send("userBook updated")
+    } catch (e){
+        console.log(e);
+    }
+})
+
+Router.delete("/:id", jwt_functions.isAuthenticated, async (request, response) => {
+    try{
+        const id = request.params.id
+        const userBook = await UserBook.findByIdAndDelete(id)
+        response.send("userBook deleted")
+    } catch (e){
+        console.log(e);
+    }
+})
+
+module.exports = Router
